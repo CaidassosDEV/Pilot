826e0bdaf16614de5c5d4adcc6a80645f11bc210
Felipe Lobas Rocha <felipelobas@localhost.localdomain>
2017-08-13 14:42:35 -0300

    Tasks - Character movement

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour {
+
+    public float maxSpeed = 10f;
+    private bool facingRight = true;
+    private Rigidbody2D rigidbody2d;
+
+    private Animator animator;
+
+    private void Awake()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void Start()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        //animator.SetFloat("vSpeed", rigidbody2d.velocity.y);
+
+        float move = Input.GetAxis("Horizontal");
+
+        animator.SetFloat("speed", Mathf.Abs(move));
+
+        rigidbody2d.velocity = new Vector2(move * maxSpeed, rigidbody2d.velocity.y);
+
+        if (move > 0 && !facingRight)
+            Flip();
+        else if (move < 0 && facingRight)
+            Flip();
+    }
+
+    private void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+
+    private Rigidbody2D rigidbody2d;
+    public float maxSpeed = 10f;
+
+    private void Awake()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+
+    void Start()
+    {
+
+    }
+
+    private void FixedUpdate()
+    {
+        float move = -0.1f;
+        rigidbody2d.velocity = new Vector2(move * maxSpeed, rigidbody2d.velocity.y);
+    }
+}
